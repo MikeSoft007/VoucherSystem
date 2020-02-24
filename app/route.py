21c83c59a076b329f2e9269b9eda6353f3f00bb5
MikeSoft007 <mekpenyong2@gmail.com>
2020-02-24 22:33:31 +0100

    changes

--- app/route.py
+++ app/route.py
@@ -7,8 +7,9 @@ from app.models import Register, random_digits, twelve_digit_serial_no
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({
-        "Hello there!": '''Welcome to this route, I AM MICHAEL here with the given priviledges you can activate a Card  with the 
-                        endpoint </activate>  and </deactivate> to deactivate a card with the given category happy testing!'''
+        "Hello there!": '''Welcome to this route, I AM MICHAEL here with the given priviledges you can activate a Card  
+                   or cards with the endpoint (/activate)  and (/deactivate) to deactivate with the given category and 
+                    card serial number HAPPY TESTING!'''
     })
 
 
